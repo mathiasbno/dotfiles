b186e20e9409f221faade52e93f33a6ed516209a
Sindre Moen <sindrenm@gmail.com>
2013-06-04 16:52:42 +0200

    Set up rbenv in zsh

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -3,3 +3,7 @@
 # --- RUBY VERSION MANAGER (RVM) -----------------------------------------------
 
 # source $HOME/.rvm/scripts/rvm
+
+# --- RBENV CONFIG -------------------------------------------------------------
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
