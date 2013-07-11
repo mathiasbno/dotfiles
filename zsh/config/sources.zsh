7326d300b46004ae3bad77fd698c29ae9551ca2c
Sindre Moen <sindrenm@gmail.com>
2013-07-11 16:09:05 +0200

    Source Node Version Manager in Zsh

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -7,3 +7,7 @@
 # --- RBENV CONFIG -------------------------------------------------------------
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# --- NODE VERSION MANAGER (NVM) -----------------------------------------------
+
+[[ -s /home/sindre/.nvm/nvm.sh ]] && . /home/sindre/.nvm/nvm.sh
