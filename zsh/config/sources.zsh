358b422211511caf73dfbfb296101dd9412670a1
Sindre Moen <sindrenm@gmail.com>
2013-07-18 12:46:03 +0200

    Remove NVM and RVM from Zsh config

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -1,13 +1,5 @@
 #! /bin/zsh
 
-# --- RUBY VERSION MANAGER (RVM) -----------------------------------------------
-
-# source $HOME/.rvm/scripts/rvm
-
 # --- RBENV CONFIG -------------------------------------------------------------
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-# --- NODE VERSION MANAGER (NVM) -----------------------------------------------
-
-[[ -s /home/sindre/.nvm/nvm.sh ]] && . /home/sindre/.nvm/nvm.sh
