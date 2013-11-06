0103b1eb534b7020252c3f57ce032872d2ff1b04
Sindre Moen <sindrenm@gmail.com>
2013-11-06 12:55:38 +0100

    Add the rbenv/shims directory to PATH
    This is done automatically by rbenv, but only once, which leads to annoying
    problems when spawning nested interactive subshells. Oooohh, nested interactive
    subshells.

--- zsh/config/environment.zsh
+++ zsh/config/environment.zsh
@@ -6,6 +6,7 @@ PATH=$HOME/.android-sdk/platform-tools:$PATH
 PATH=/usr/local/texlive/2013/bin/x86_64-linux:$PATH
 PATH=/usr/local/heroku/bin:$PATH
 PATH=$HOME/.rbenv/bin:$PATH;
+PATH=$HOME/.rbenv/shims:$PATH;
 PATH=$HOME/bin:$PATH;                                       export PATH
 
 MANPATH=/usr/local/texlive/2012/texmf/doc/man:$MANPATH;     export MANPATH
