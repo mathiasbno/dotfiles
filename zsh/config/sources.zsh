0023ee250e18804e1438ef434bcb7427bced633d
Sindre Moen <sindrenm@gmail.com>
2014-01-19 09:20:37 +0100

    Source Zsh antigen if it exists

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -3,3 +3,7 @@
 # --- RBENV CONFIG -------------------------------------------------------------
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+if [ -e /usr/share/zsh/scripts/antigen/antigen.zsh ] ; then
+  source /usr/share/zsh/scripts/antigen/antigen.zsh
+fi
