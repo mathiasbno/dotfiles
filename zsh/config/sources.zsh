9f472406e68e337534ba3d33606d9560c2c67359
Sindre Moen <sindrenm@gmail.com>
2015-08-26 10:11:43 +0200

    Source ndenv in shell

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -7,3 +7,7 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 # --- DIRENV CONFIG ------------------------------------------------------------
 
 eval "$(direnv hook zsh)"
+
+# --- NDENV CONFIG -------------------------------------------------------------
+
+if which ndenv > /dev/null; then eval "$(ndenv init -)"; fi
