1981cbb8e2beb83f41a4ee9095c67469baaddadc
Sindre Moen <sindrenm@gmail.com>
2015-08-26 10:10:23 +0200

    Remove nvm source

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -7,6 +7,3 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 # --- DIRENV CONFIG ------------------------------------------------------------
 
 eval "$(direnv hook zsh)"
-
-# --- NVM EXECUTABLE -----------------------------------------------------------
-source /usr/share/nvm/init-nvm.sh
