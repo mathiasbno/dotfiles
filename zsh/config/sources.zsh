421204ab564ad319db23fd9e6a088dd43d790e56
Sindre Moen <sindrenm@gmail.com>
2015-09-18 10:49:34 +0200

    Remove hard-coded path to Antigen

--- zsh/config/sources.zsh
+++ zsh/config/sources.zsh
@@ -4,11 +4,6 @@
 
 eval "$(direnv hook zsh)"
 
-# --- CHRUBY -------------------------------------------------------------------
-
-source /usr/share/chruby/chruby.sh
-source /usr/share/chruby/auto.sh
-
 # --- NDENV CONFIG -------------------------------------------------------------
 
 if which ndenv > /dev/null; then eval "$(ndenv init -)"; fi
